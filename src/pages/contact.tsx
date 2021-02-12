5fe6098e52023ba1fb6f1b75d79bb33329e069b5
Wataru Manji <manji@linux.com>
2021-02-13 04:55:56 +0900

    Update Avatar

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -13,22 +13,22 @@ const contacts = [
   {
     Comp: Twitter,
     alt: 'twitter icon',
-    link: 'https://twitter.com/_ijjk',
+    link: 'https://twitter.com/_manji0',
   },
   {
     Comp: GitHub,
     alt: 'github icon',
-    link: 'https://github.com/ijjk',
+    link: 'https://github.com/manji-0',
   },
   {
     Comp: LinkedIn,
     alt: 'linkedin icon',
-    link: 'https://www.linkedin.com/in/jj-kasper-0b5392166/',
+    link: 'https://www.linkedin.com/in/manji0',
   },
   {
     Comp: Envelope,
     alt: 'envelope icon',
-    link: 'mailto:jj@jjsweb.site?subject=Notion Blog',
+    link: 'mailto:manji@linux.com',
   },
 ]
 
@@ -37,15 +37,12 @@ export default () => (
     <Header titlePre="Contact" />
     <div className={sharedStyles.layout}>
       <div className={contactStyles.avatar}>
-        <img src="/avatar.png" alt="avatar with letters JJ" height={60} />
+        <img src="/avatar.jpg" alt="avatar with Wataru Manji" height={60} />
       </div>
 
       <h1 style={{ marginTop: 0 }}>Contact</h1>
 
-      <div className={contactStyles.name}>
-        JJ Kasper - Next.js Engineer @{' '}
-        <ExtLink href="https://vercel.com">Vercel</ExtLink>
-      </div>
+      <div className={contactStyles.name}>Wataru Manji</div>
 
       <div className={contactStyles.links}>
         {contacts.map(({ Comp, link, alt }) => {
