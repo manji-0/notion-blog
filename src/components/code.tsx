e8f6541c39cc42bbbe71a28b39264af1a43e7a50
Wataru Manji <manji@linux.com>
2021-02-23 13:44:35 +0900

    fix hilight

--- src/components/code.tsx
+++ src/components/code.tsx
@@ -1,4 +1,5 @@
 import Prism from 'prismjs'
+import 'prismjs/components/prism-bash'
 import 'prismjs/components/prism-jsx'
 import 'prismjs/components/prism-python'
 import 'prismjs/components/prism-diff'
